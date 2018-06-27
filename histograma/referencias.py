25b6f6a1efb86a6a20472822aa5d7cdcfb17db8d
Daniel de Queiroz <31702467+dqb2017@users.noreply.github.com>
2018-06-26 23:12:14 -0300

    Add files via upload

---
+++ histograma/referencias.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+.
++---------------------------------------------------+
+|  Referencias:                                     |
++---------------------------------------------------+
+
+- CI240/CI182 - Fundamentos de Programação.
+    Sitio <http://www.inf.ufpr.br/cursos/ci240/>.
+    Acesso em 23/06/2018. 
+
+- Python Brasil. AprendaProgramar.
+    Sitio <https://wiki.python.org.br/AprendaProgramar>.
+    Acesso em 23/06/2018.
+
+- Python - O básico da sintaxe.
+    Sitio <http://www.devfuria.com.br/python/sintaxe-basica/>.
+    Acesso em 23/06/2018.
+
+- Python 3.6.5 Documentation. Ajuda do IDLE. 
+
+- Portal Action. HISTOGRAMA.
+    Sítio <http://www.portalaction.com.br/estatistica-basica/16-histograma>.
+    Acesso em 23/06/2018.
+
+- Blog da Qualidade. Calculo de limites das classes.
+    Sitio <http://www.blogdaqualidade.com.br/histograma/>.
+    Acesso em 23/06/2018.
+
+- The Python Tutorial — Python 3.6.6 documentation.
+    Site <https://docs.python.org/3/tutorial/>.
+    Acesso em 24/06/2018.
+
+- The Python Tutorial. List Comprehension.
+    Sitio <https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions>.
+    Acesso em 24/03/2018. 
+
+- WikiPedia. Compreensão de lista.
+    Sitio <https://pt.wikipedia.org/wiki/Compreens%C3%A3o_de_lista>.
+    Acesso em 26/05/2018.
+
+- YouTube. Por que todos deveriam aprender a programar? Legendado. 5min.
+    Video <https://www.youtube.com/watch?v=mHW1Hsqlp6A>.
+    Acesso em 23/06/2018. 
+	
+===================================================
+"""
+def main():
+    print(__doc__) 
+
+def doc():
+    print(__doc__) 
